9413091c13d52f0ef84dcff9d278d16024216f2c
Deaadman <47807486+Deaadman@users.noreply.github.com>
2026-01-09 10:04:52 +1030

    Fixed nightly release versioning

--- Source/BuildInfo.g.cs
+++ Source/BuildInfo.g.cs
@@ -3,7 +3,7 @@ namespace ItemRarities.Properties;
 internal static class BuildInfo
 {
 	public const string Name = "Item Rarities";
-	public const string Version = "";
+	public const string Version = "2.0.5";
 	public const string Author = "Deadman";
 	public const string DownloadLink = "https://github.com/Deaadman/item-rarities/releases/latest/download/ItemRarities.dll";
 	public const int Priority = 0;
